3507ef37e57483354b92241f27bdd8b8472902ae
Manh Quang <quangdm@newwave.vn>
2025-11-20 15:26:17 +0700

    init file

--- src/BuildingBlocks/Contracts/obj/Debug/net9.0/Contracts.AssemblyInfo.cs
+++ src/BuildingBlocks/Contracts/obj/Debug/net9.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd5a15570244abf1c29c7c3b8b2035eaf665b64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13cd80269190311616bfdef6b73fa776a7322b5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
